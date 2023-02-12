03d08b342ebf583ff2c82d2dc9a0c673f3412c37
Yushi Yang <yangyushi1992@icloud.com>
2023-02-13 00:08:20 +0800

    Chat #7 MVC
    The code is generally great! But the overall design can be improved, can you rewrite the code following the MVC principle?

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import sys
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QComboBox, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QRadioButton, QLineEdit, QWidget
+from PyQt5.QtCore import QObject, pyqtSignal
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
-class PlotWindow(QMainWindow):
+class Model(QObject):
+    data_loaded = pyqtSignal(pd.DataFrame)
+    
     def __init__(self):
         super().__init__()
+        self.df = None
+    
+    def load_data(self, file_name):
+        self.df = pd.read_excel(file_name)
+        self.data_loaded.emit(self.df)
+    
+    def get_data(self):
+        return self.df
+    
+class View(QMainWindow):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
         self.initUI()
         
     def initUI(self):
@@ -17,10 +32,7 @@ class PlotWindow(QMainWindow):
         self.setGeometry(100, 100, 800, 600)
         
         self.load_button = QPushButton('Load', self)
-        self.load_button.clicked.connect(self.load_data)
-        
         self.plot_button = QPushButton('Plot', self)
-        self.plot_button.clicked.connect(self.plot_data)
         self.plot_button.setEnabled(False)
         
         self.line_plot_button = QRadioButton("Line Plot", self)
@@ -60,48 +72,62 @@ class PlotWindow(QMainWindow):
         central_widget = QWidget(self)
         central_widget.setLayout(vbox)
         self.setCentralWidget(central_widget)
+
+class Controller(QObject):
+    def __init__(self, model, view):
+        super().__init__()
+        self.model = model
+        self.view = view
+        self.view.load_button.clicked.connect(self.load_data)
+        self.view.plot_button.clicked.connect(self.plot_data)
+        self.model.data_loaded.connect(self.data_loaded)
         
     def load_data(self):
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
-        file_name, _ = QFileDialog.getOpenFileName(self, "Load Data", "", "Excel Files (*.xlsx);;All Files (*)", options=options)
+        file_name, _ = QFileDialog.getOpenFileName(self.view, "Load Data", "", "Excel Files (*.xlsx);;All Files (*)", options=options)
         if file_name:
-            self.df = pd.read_excel(file_name)
-            self.x_combo.clear()
-            self.y_combo.clear()
-            self.x_combo.addItems(list(self.df.columns))
-            self.y_combo.addItems(list(self.df.columns))
-            self.x_combo.setEnabled(True)
-            self.y_combo.setEnabled(True)
-            self.y_transform_edit.setEnabled(True)
-            self.plot_button.setEnabled(True)
+            self.model.load_data(file_name)
+    
+    def data_loaded(self, df):
+        self.view.x_combo.clear()
+        self.view.y_combo.clear()
+        self.view.x_combo.addItems(list(df.columns))
+        self.view.y_combo.addItems(list(df.columns))
+        self.view.x_combo.setEnabled(True)
+        self.view.y_combo.setEnabled(True)
+        self.view.y_transform_edit.setEnabled(True)
+        self.view.plot_button.setEnabled(True)
     
     def plot_data(self):
-        x = self.x_combo.currentText()
-        y = self.y_combo.currentText()
-        y_transform = self.y_transform_edit.text()
+        x = self.view.x_combo.currentText()
+        y = self.view.y_combo.currentText()
+        y_transform = self.view.y_transform_edit.text()
+        df = self.model.get_data()
         if y_transform:
             try:
                 y_transform = eval(f"lambda y: {y_transform}")
-                y_data = y_transform(self.df[y])
+                y_data = y_transform(df[y])
             except:
-                y_data = self.df[y]
+                y_data = df[y]
         else:
-            y_data = self.df[y]
-        self.figure.clear()
-        ax = self.figure.add_subplot(111)
-        if self.line_plot_button.isChecked():
-            ax.plot(self.df[x], y_data)
+            y_data = df[y]
+        self.view.figure.clear()
+        ax = self.view.figure.add_subplot(111)
+        if self.view.line_plot_button.isChecked():
+            ax.plot(df[x], y_data)
         else:
-            ax.scatter(self.df[x], y_data)
+            ax.scatter(df[x], y_data)
         ax.set_xlabel(x)
         ax.set_ylabel(y)
-        self.figure.tight_layout()
-        self.canvas.draw()
+        self.view.figure.tight_layout()
+        self.view.canvas.draw()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = PlotWindow()
-    window.show()
+    model = Model()
+    view = View(model)
+    Controller(model, view)
+    view.show()
     sys.exit(app.exec_())
 
